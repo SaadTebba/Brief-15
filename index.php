663013eea07221c9a706115bfe76c0f0e18aec2f
SaadTebba <saad.tebba.solicode@gmail.com>
2023-02-21 12:38:27 +0100

    ===

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include 'connection.php' ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,20 +24,47 @@
     <!-- ================================= Header (100vh) ================================= -->
 
     <header class="p-3 bg-white">
-        <!-- <div class="d-flex justify-content-start">
-            <img src="logo.png" alt="logo">
-        </div> -->
-        <div class="d-flex justify-content-end gap-2">
-            <button class="btn signin"><span class="h6">SIGN IN</span></button>
-            <button class="btn btn-primary signup"><span class="h6">SIGN UP</span></button>
+
+        <div class="row">
+
+            <div class="col-3">
+                <img src="logo.png" alt="logo" class="w-50">
+            </div>
+
+            <div class="col-6">
+                <form method="GET">
+                    <div class="input-group">
+                        <div class="form-outline">
+                            <input type="search" id="search" class="form-control">
+                        </div>
+                        <button type="button" class="btn"><i class="fas fa-search filtersearch"></i></button>
+                        <button type="button" class="btn"><i class="fa-solid fa-filter filtersearch"></i></button>
+                    </div>
+                </form>
+            </div>
+
+            <div class="col-3 d-flex justify-content-end gap-2">
+                <button class="btn signin"><span class="h6">SIGN IN</span></button>
+                <button class="btn btn-primary signup"><span class="h6">SIGN UP</span></button>
+            </div>
+
         </div>
+
     </header>
+
     <div id="image">
         <h1 class="h1 text-center container pt-5 display-1 fw-normal">Buy, rent and sell your properties easily with us!</h1>
     </div>
+
     <div class="down-arrow"></div>
 
     <!-- ================================= Cards container ================================= -->
+
+    <select class="form-select btn sortingSelect">
+        <option selected disabled>Sort by</option>
+        <option value="1">Publication date</option>
+        <option value="2">Price</option>
+    </select>
 
     <div class="container p-5 text-center">
 
@@ -44,46 +73,8 @@
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn detailsButton" data-bs-toggle="modal" data-bs-target="#ModalWindow">Details</a>
-            </div>
-        </div>
-        <div class="card d-inline-block m-1" style="width: 18rem;">
-            <img src="announceimg.jfif" class="card-img-top" alt="announceImage">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn detailsButton" data-bs-toggle="modal" data-bs-target="#ModalWindow">Details</a>
-            </div>
-        </div>
-        <div class="card d-inline-block m-1" style="width: 18rem;">
-            <img src="announceimg.jfif" class="card-img-top" alt="announceImage">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn detailsButton" data-bs-toggle="modal" data-bs-target="#ModalWindow">Details</a>
-            </div>
-        </div>
-        <div class="card d-inline-block m-1" style="width: 18rem;">
-            <img src="announceimg.jfif" class="card-img-top" alt="announceImage">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn detailsButton" data-bs-toggle="modal" data-bs-target="#ModalWindow">Details</a>
-            </div>
-        </div>
-        <div class="card d-inline-block m-1" style="width: 18rem;">
-            <img src="announceimg.jfif" class="card-img-top" alt="announceImage">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn detailsButton" data-bs-toggle="modal" data-bs-target="#ModalWindow">Details</a>
-            </div>
-        </div>
-        <div class="card d-inline-block m-1" style="width: 18rem;">
-            <img src="announceimg.jfif" class="card-img-top" alt="announceImage">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text">For rent</p>
+                <p class="card-text">15$/h</p>
                 <a href="#" class="btn detailsButton" data-bs-toggle="modal" data-bs-target="#ModalWindow">Details</a>
             </div>
         </div>
@@ -92,7 +83,7 @@
 
     <!-- ================================= Modal window ================================= -->
 
-    <div class="modal fade" id="ModalWindow" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal modal-lg fade" id="ModalWindow" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
