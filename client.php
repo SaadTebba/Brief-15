ebfd943817f470e9829f2cb46b8f6bf5cf33e2c5
Saad Tebba <116269919+SaadTebba@users.noreply.github.com>
2023-02-23 13:40:38 +0100

    ===

--- client.php
+++ client.php
@@ -16,9 +16,48 @@
 
 <body>
 
-    <!-- ================================= Cards container ================================= -->
+    <!--=================================Header (100vh)=================================-->
 
-    <?php include 'header.php' ?>
+    <header class="p-3 bg-white">
+
+        <div class="row">
+
+            <div class="col-3">
+                <img src="logo.png" alt="logo" class="w-50">
+            </div>
+
+            <div class="col-6">
+                <form method="POST">
+                    <div class="input-group">
+                        <input type="search" name="search" id="search" class="form-control" placeholder="Search">
+                        <select class="border" name="filter_search">
+                            <option value="All">All</option>
+                            <option value="City">City</option>
+                            <option value="Category">Category</option>
+                            <option value="Type">Type</option>
+                            <option value="Price">Price</option>
+                        </select>
+                        <button type="submit" class="btn searchbtn border" title="Search"><i class="fas fa-search filtersearch"></i></button>
+                    </div>
+                </form>
+            </div>
+
+            <div class="col-3 d-flex justify-content-end gap-2 profileClick">
+                <img src="clientimg.png" alt="Profile img">
+                <i class="fa-solid fa-chevron-down my-auto"></i>
+            </div>
+
+        </div>
+
+    </header>
+
+    <div id="image">
+        <h1 class="h1 text-center container pt-5 display-1 fw-normal">Buy, rent and sell your properties easily with us!</h1>
+    </div>
+
+    <div class="down-arrow" onclick="scrollDown()"></div>
+
+    <!-- ================================= Cards container ================================= -->
 
     <form method="POST">
         <div class="input-group">
@@ -124,7 +163,7 @@
     // $statement = $conn->prepare();
     ?>
 
-    <div class="modal modal-lg fade" id="ModalWindow" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal modal-lg fade" id="ModalWindow" tabindex="-1" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
